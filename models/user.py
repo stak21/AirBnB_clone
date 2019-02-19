d1ce50fe337978ef97d71b6378425fbefb38267e
--get <angiedelgado08@gmail.com>
2019-02-19 19:05:36 +0000

    changes to user

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ class User(BaseModel):
 
     def __init__(self, email, password, first_name, last_name):
         """defining public instance attributes"""
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
