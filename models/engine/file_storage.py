5b4acd8b5467d120a0da2b044a0e83f6a1fdefef
--get <angiedelgado08@gmail.com>
2019-02-19 01:14:19 +0000

    Changed all self to FileStorage Fixed new and save reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 import json
 
 
@@ -11,36 +10,31 @@ class FileStorage():
 
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """set __objects the obj with key <obj class name>.id
         & adds the new dictionary to __objects"""
-        if isinstance(obj, BaseModel):
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            self.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj.to_dict()
 
     def save(self):
         """serializes __objects to the JSON file (__file_path)"""
-        with open(self.__file_path, 'w', encoding="utf-8") as f:
-            for key, value in self.__objects.items():
-                self.__objects[key] = value.to_dict()
-            json.dump(self.__objects, f)
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
+            json.dump(FileStorage.__objects, f)
 
     def reload(self):
         """if file exists, public instance method deserializes the JSON file
         to __objects"""
         try:
-            with open(self.__file_path) as f:
-                instdict = json.load(f)
-            for key, value in instdict.items():
-                key = key.split(".")
-                self.__objects = eval('{}(**value)'.format(key[0]))
-        except FileNotFoundError:
+            with open(FileStorage.__file_path) as f:
+                FileStorage.__objects = json.load(f)
+        except:
              pass
 
     @classmethod
     def _refresh(cls):
         """refresh the __object to an empty dict."""
-        cls.__object = {}
-print('hi')
+        FileStorage.__object = {}
+        with open("file.json", 'w') as f:
+            json.dump(FileStorage.__object, f)
