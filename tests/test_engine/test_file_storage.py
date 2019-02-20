a295ed9b6c05c58609693f97d5a7a5fc5f34aa5c
--get <angiedelgado08@gmail.com>
2019-02-20 08:41:26 +0000

    moved test file

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-UnitTest for filestorage: This class manages objects in storage letting you
-save, reload, and create
-"""
+""" UnitTest for filestorage """
 import unittest
 import os
 import json
