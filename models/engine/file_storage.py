1e83e9899aa836e2698a8c68af90aca459784b43
--get <angiedelgado08@gmail.com>
2019-02-20 05:32:00 +0000

    changed all FileStorage to self

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,31 +11,31 @@ class FileStorage():
 
     def all(self):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """set __objects the obj with key <obj class name>.id
         & adds the new dictionary to __objects"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj.to_dict()
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
         """serializes __objects to the JSON file (__file_path)"""
-        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
-            json.dump(FileStorage.__objects, f)
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            json.dump(self.__objects, f)
 
     def reload(self):
         """if file exists, public instance method deserializes the JSON file
         to __objects"""
         try:
-            with open(FileStorage.__file_path) as f:
-                FileStorage.__objects = json.load(f)
+            with open(self.__file_path) as f:
+                self.__objects = json.load(f)
         except:
             pass
 
     @classmethod
     def _refresh(cls):
         """refresh the __object to an empty dict."""
-        FileStorage.__object = {}
+        cls.__objects = {}
         with open("file.json", 'w') as f:
-            json.dump(FileStorage.__object, f)
+            json.dump(cls.__objects, f)
