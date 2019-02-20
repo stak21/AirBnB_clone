27cfdf0c4c716ed90f23699c6782f28765b331b7
--get <angiedelgado08@gmail.com>
2019-02-20 08:11:17 +0000

    Changed kwargs condition and storage call; removed self from to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -2,20 +2,20 @@
 """ Class: Base Model """
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel():
     """ A baseclass for future subclasses """
     def __init__(self, *args, **kwargs):
         """ Instantiates the base model variables """
-        if len(kwargs) > 0:
+        if kwargs:
             self.__dict__ = kwargs
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """ create a string that prints class name, id, dict """
@@ -25,12 +25,13 @@ class BaseModel():
     def save(self):
         """ Updates the updated_at variable with the current time """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
-        self.dic = {key: val for key, val in self.__dict__.items()}
-        self.dic['updated_at'] = str(self.dic['updated_at'])
-        self.dic['created_at'] = str(self.dic['created_at'])
-        self.dic['__class__'] = self.__class__.__name__
-        return self.dic
+        dic = {key: val for key, val in self.__dict__.items()}
+        dic['updated_at'] = str(dic['updated_at'])
+        dic['created_at'] = str(dic['created_at'])
+        dic['__class__'] = self.__class__.__name__
+        
+        return dic
