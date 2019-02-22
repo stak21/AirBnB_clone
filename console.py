a19ea6310fca19b3b32e8aff1c50486af0b924b4
stak21 <stakashima21@gmail.com>
2019-02-22 00:30:46 +0000

    added precmd

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import cmd
+import re
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -22,20 +23,62 @@ class HBNBCommand(cmd.Cmd):
                 "Amenity",
                 "Review"
                 ]
-# adding the classes to this list so the methods check throgh here if class
-# exists.
+
+    def precmd(self, line):
+        storage.reload()
+        regex = "[^()., {:\"}']+"
+        matches = re.finditer(regex, line, re.MULTILINE)
+        all_group = []
+        cmd = cls = uid = ""
+        for match in matches:
+            all_group.append(match.group())
+        if len(all_group) == 1:
+            cmd = all_group
+        elif len(all_group) == 2:  # assigns only if correct number of args
+            cmd = all_group.pop(0)
+            cls = all_group.pop(0)
+        elif len(all_group) >= 3:
+            cmd = all_group.pop(0)
+            cls = all_group.pop(0)
+            uid = all_group.pop(0)
+        if '.' in line:
+            cmd, cls = cls, cmd
+        if '{' in line or cmd == "update":
+            for key, value in zip(all_group[0::2], all_group[1::2]):
+                self.do_update(cls, uid, key, value)
+            return 'emptyline'
+        if cls in self.list_classes:
+            line = "{} {} {}".format(cmd, cls, uid)
+        return line
 
     def do_quit(self, arg):
         """Quit command to exit the program\n"""
         return True
 
-    def do_EOF():
+    def do_EOF(self, *args):
         """End of File to exit file."""
         print()
 
-    def emptyline(self):
+    def do_emptyline(self, arg):
         """method so it should not execute anything"""
         pass
+
+    def do_count(self, *args):
+        count = 0
+        args = [ele for ele in args[0].split(' ')]
+        if args[0] == '':
+            print("** class name missing **")
+            return
+        if args[0] not in self.list_classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            ''' Get a list of specified instances '''
+            for key, obj in storage.all().items():
+                key = key.split('.')
+                if key[0] == args[0]:
+                    count += 1
+            print(count)
 
     def do_create(self, *args):
         """Creates new instance of BaseModel & saves to json file & prints
@@ -121,7 +164,6 @@ class HBNBCommand(cmd.Cmd):
             return
         else:
             ''' Get a list of specified instances '''
-            print(storage.all().items())
             for key, obj in storage.all().items():
                 key = key.split('.')
                 if key[0] == args[0]:
@@ -130,7 +172,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, *args):
         """update/adds attributes in an instance based on class and id."""
-        args = [ele for ele in args[0].split(' ')]
+        print(args)
         if args[0] == '':
             print("** class name missing **")
             return
@@ -145,7 +187,6 @@ class HBNBCommand(cmd.Cmd):
             return
         elif len(args) < 4:
             print("** value missing **")
-            return
 
         storage.reload()
         dict_objs = storage.all()
@@ -156,7 +197,15 @@ class HBNBCommand(cmd.Cmd):
         key = "{}.{}".format(args[0], args[1])
         if key in dict_objs.keys():
             obj = dict_objs[key]
-            obj.__dict__[args[2]] = args[3]
+            print(args[2])
+            print(obj.__dict__)
+            if args[2] in obj.__class__.__dict__:
+                print('exists key')
+                obj.__dict__[args[2]] =\
+                    type(obj.__class__.__dict__[args[2]])(args[3])
+            else:
+                print('no exists key')
+                obj.__dict__[args[2]] = args[3]
             storage.save()
         else:
             print("** no instance found **")
